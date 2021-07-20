7d6de9b26c916ad72b8772b48171e02c40c0a174
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2021-07-20 09:45:37 +0200

    Scenario Statistics now should also include initial Transporter Positions

--- src/main/java/de/dfki/StigLD/Benchmark/ScenarioStatistics.java
+++ src/main/java/de/dfki/StigLD/Benchmark/ScenarioStatistics.java
@@ -5,8 +5,11 @@
  */
 package de.dfki.StigLD.Benchmark;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 
 /**
@@ -18,17 +21,34 @@ public class ScenarioStatistics {
     public int numOrders;
     public int numWorkstations;
     public int numTransporters;
+    public Map<String, TransporterStatistics> initialPositions = new HashMap<>();
 
     public ScenarioStatistics() {
     }
 
     public void read(String endpoint) {
+	readArtifactCounts(endpoint);
+	readInitialTransporterPositions(endpoint);
+    }
+
+    private void readArtifactCounts(String endpoint) {
 	ResultSet results = QueryExecutionFactory.sparqlService(endpoint, QueryFactory.create(Queries.count_artifacts)).execSelect();
 	while (results.hasNext()) {
-	    var line = results.next();
+	    QuerySolution line = results.next();
 	    numOrders = line.getLiteral("orders").getInt();
 	    numWorkstations = line.getLiteral("workstations").getInt();
 	    numTransporters = line.getLiteral("transporters").getInt();
+	}
+    }
+
+    private void readInitialTransporterPositions(String endpoint) {
+	ResultSet results = QueryExecutionFactory.sparqlService(endpoint, QueryFactory.create(Queries.transporter_pos)).execSelect();
+	while (results.hasNext()) {
+	    QuerySolution line = results.next();
+	    String id = line.getResource("t").getLocalName();
+	    int x = line.getLiteral("x").getInt();
+	    int y = line.getLiteral("y").getInt();
+	    initialPositions.put(id, new TransporterStatistics(x, y, 0));
 	}
     }
 }
